9a2aee181c56fc156c46c77c8609cd73db86782e
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-02-12 13:57:12 -0500

    Fix sh typo

--- turntable.sh
+++ turntable.sh
@@ -7,8 +7,8 @@ fi
 if [[ ! -z $3 ]]; then
     steps="--turntable-steps $3"
 fi
+input_file=$1
 shift
 shift
 shift
-echo $@
-blender turntable.blend --window-geometry 0 0 1 1 --python turntable.py -- --turntable-model $1 ${output} ${steps} $@
+blender turntable.blend --window-geometry 0 0 1 1 --python turntable.py -- --turntable-model ${input_file} ${output} ${steps} $@
